8e4e388a0aed7e919ea28d5a5430063aa9650df1
Moritz Meisel <mmeisel@thoughtworks.com>
2017-06-12 15:45:23 +0200

    added styling and background images for card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,71 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Suits, Faces } from '../constants/Cards';
+import './Card.css';
 
 export default class Card extends React.Component {
 
+    getColor(card) {
+        return (card.suit === Suits.hearts || card.suit === Suits.diamonds)
+            ? 'red' : 'black';
+    }
+
+    getBackgroundImage(card) {
+
+        const suitIndex = this.getSuitIndex(card.suit);
+        const faceIndex = this.getFaceIndex(card.face);
+
+        return suitIndex + '_' + faceIndex + '.svg';
+    }
+
+    getFaceIndex(face) {
+        switch(face) {
+            case Faces.two: return 2;
+            case Faces.three: return 3;
+            case Faces.four: return 4;
+            case Faces.five: return 5;
+            case Faces.six: return 6;
+            case Faces.seven: return 7;
+            case Faces.Q: return 12;
+            case Faces.J: return 11;
+            case Faces.K: return 13;
+            default: return 0;
+        }
+    }
+
+    getSuitIndex(suit) {
+        switch(suit) {
+            case Suits.spades: return 0;
+            case Suits.hearts: return 1;
+            case Suits.clubs: return 2;
+            case Suits.diamonds: return 3;
+            default: return 0;
+        }
+    }
+
     render() {
+        const card = this.props.card;
+
+        const color = this.getColor(this.props.card);
+        const backgroundImage = '/faces/' + this.getBackgroundImage(card);
         const Button = styled.button`
-            border-radius: 3px;
-            padding: 0.25em 1em;
+            position: relative;
+            border-radius: 8px;
             margin: 0 1em;
+            height: 30em;
+            width: 20em;
             background: transparent;
-            color: palevioletred;
-            border: 2px solid palevioletred;
+            color: ${color};
+            border: 2px solid ${color};
+            background-image: url(${backgroundImage});
+            background-size: 20em 30em;
         `
 
-        const card = this.props.card;
         return (
-            <Button onClick={() => this.props.submitCard(card)}>{card.toString()}</Button>
+            <Button onClick={() => this.props.submitCard(card)}>
+                <div className="face top">{card.face}</div>
+                <div className="face bottom">{card.face}</div>
+            </Button>
         );
     }
 }
